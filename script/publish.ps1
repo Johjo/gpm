104146220e2c604be5223221033b18cb572ff89a
Jean-Marc Le Roux <jeanmarc.leroux@aerys.in>
2018-07-13 20:55:37 +0200

    Fix broken reference to the 7zip binary.

--- script/publish.ps1
+++ script/publish.ps1
@@ -1,20 +1,21 @@
-if (-Not $env:APPVEYOR_REPO_TAG) {
-    Write-Host "Not a tag: skip publishing."
-    exit 0    
-}
+# if (-Not $env:APPVEYOR_REPO_TAG) {
+#     Write-Host "Not a tag: skip publishing."
+#     exit 0    
+# }
 
 if ($env:target -ne "x86_64-pc-windows-msvc") {
     Write-Host "Target $env:target detected: skip publishing."
     exit 0
 }
 
-$version = $env:APPVEYOR_REPO_TAG_NAME
+# $version = $env:APPVEYOR_REPO_TAG_NAME
+$version = "0.5.0"
 $github_username = $env:GITHUB_USERNAME
 $github_token = $env:GITHUB_TOKEN
 
 & { $env:GIT_LFS_SKIP_SMUDGE=1; git clone "https://${github_username}:${github_token}@github.com/aerys/gpm-packages.git" 2>&1 | Write-Host }
 mkdir -Force -p gpm-packages/gpm-windows64
-7za.exe a -ttar -so archive.tar .\target\release\gpm.exe | 7za.exe a -si .\gpm-packages\gpm-windows64\gpm-windows64.tar.gz
+7z a -ttar -so archive.tar .\target\release\gpm.exe | 7z a -si .\gpm-packages\gpm-windows64\gpm-windows64.tar.gz
 git config --global user.email "noreply@ci.appveyor.com" 2>&1 | Write-Host
 git config --global user.name "AppVeyor" 2>&1 | Write-Host
 git add gpm-windows64.tar.gz 2>&1 | Write-Host
